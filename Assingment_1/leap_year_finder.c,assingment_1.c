901a9b5ca754b16542f3521fadfa9e028daa79b0
meb25051-creator <meb25051@tezu.ac.in>
2025-11-21 21:02:11 +0530

    Update leap_year_finder.c,assingment_1.c

--- Assingment_1/leap_year_finder.c,assingment_1.c
+++ Assingment_1/leap_year_finder.c,assingment_1.c
@@ -1,17 +1,26 @@
 #include <stdio.h>
-int main(){
-	int year;//code to find leap year,leap year is amultiple of 4 or 400
-	printf("Enter your desired year: ");
-	scanf("%d",&year);
-	if(year %4 == 0){
-		printf("The following year,%d is a leap year",year);
-	}
-	else if(year %400 == 0){
-		printf("The following year,%d is a leap year",year);
-	}
-	else{
-		printf("The following year,%d is not a leap year",year);
-	}
-	return 0;
+
+int main() {
+    int year;
+
+    printf("Enter your desired year: ");
+    scanf("%d", &year);
+
+    if (year % 400 == 0) {
+        printf("%d is a leap year", year);
+    }
+    else if (year % 100 == 0) {
+        printf("%d is not a leap year", year);
+    }
+    else if (year % 4 == 0) {
+        printf("%d is a leap year", year);
+    }
+    else {
+        printf("%d is not a leap year", year);
+    }
+
+    return 0;
 }
+
+
 	
